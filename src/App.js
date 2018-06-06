d4e89cad10a213ec6d324e54aa306da1ff1ef2c0
jessicawatsonmiller <jessicawatsonmiller@gmail.com>
2018-06-05 22:05:20 -0700

    Add all files

---
+++ src/App.js
@@ -0,0 +1,67 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+        <p>
+        Resum&eacute; <a href="mailto:jessicawatsonmiller@gmail.com">Contact me</a>
+        </p>
+        <p>
+        JESSICA WATSON MILLER
+        </p>
+        <p>
+        <a href="https://twitter.com/jesswatmiller">Twitter</a> | <a href="https://www.linkedin.com/in/jessica-watson-miller-0082aa60/">Linkedin</a> | <a href="https://www.instagram.com/art_kinetica/">Instagram</a>
+        </p>
+        <p>
+        WHO ARE YOU AND WHAT DO YOU WANT?
+        </p>
+        <p>
+        I&#39;m Jess.
+        </p>
+        <p>
+        I want to help humans coordinate better, and foster humanity’s transition to a metasystematic paradigm.
+        </p>
+        <p>
+        What even is a metasystematic paradigm, you ask? <br/>
+        A way of being that recognises that meaning is both <a href="https://vividness.live/2015/10/12/developing-ethical-social-and-cognitive-competence/">nebulous and patterned</a>. <br/>
+        That&#39;s probably still confusing. <a href="https://www.lesswrong.com/posts/6if3cGi3gEiaDg5Bo/metarationality-repository">Sorry about that</a>.
+        </p>
+        <p>
+        I like cryptoeconomics, software and scalable social technologies. <br/>
+        I’m technique-agnostic, I learn fast and love to work with people who are truly alive.
+        </p>
+        <p>
+        I&#39;m a pretty multi-faceted human, as far as humans go: <br/>
+        I&#39;ve written things, with varying degrees of professionalism, on <a href="https://medium.com/@jessicawatsonmiller">Medium</a>. <br/>
+        I created the world&#39;s first <a href="https://laureneisingerproductions.com/luminous/">blacklight circus show</a>. <br/>
+        I once competed on a <a href="https://en.wikipedia.org/wiki/Skin_Wars#Season_3">reality TV show</a> about bodypainting. <br/>
+        (Yes, it&#39;s on <a href="https://www.netflix.com/title/80109536">Netflix</a>.) <br/>
+        Oh yeah, I&#39;m also a <a href="http://artkinetica.com.au/">bodypainter</a>. <br/>
+        </p>
+        <p>
+        Want to reach me? <br/>
+        Send me a message on <a href="https://www.facebook.com/jessicawatsonmiller">Facebook</a>, or schedule a <a href="https://calendly.com/jessicawatsonmiller">hangout</a> with me. <br/>
+        Or, if it isn&#39;t urgent, send me an <a href="mailto:jessicawatsonmiller@gmail.com">email</a>. <br/>
+        </p>
+        <p>
+        It&#39;s nice to meet you.
+        </p>
+        <p>
+        Website by me. Artwork by Vesod Brero.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
